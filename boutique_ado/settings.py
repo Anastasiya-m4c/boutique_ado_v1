e4a78101c8d81c4183053fff344d723e97f17aa7
annie-mac <mcavoy.ana@gmail.com>
2025-08-20 20:14:11 +0100

    set up allauth part1

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'allauth',  # Django Allauth for authentication
+    'allauth.account',  # Django Allauth account management
+    'allauth.socialaccount',  # Django Allauth social account management
 ]
 
 MIDDLEWARE = [
@@ -59,13 +62,21 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request',
+                'django.template.context_processors.request', # 
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
         },
     },
 ]
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by email
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+SITE_ID = 1
 
 WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 
